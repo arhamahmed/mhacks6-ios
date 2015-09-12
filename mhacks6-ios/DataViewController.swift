4356045014fc6808bac3ab80ff780ab2047af635
Arham Ahmed <a.arhamIT@gmail.com>
2015-09-12 09:52:09 -0400

    Initial Commit

---
+++ mhacks6-ios/DataViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DataViewController.swift
+//  mhacks6-ios
+//
+//  Created by Arham Ahmed on 2015-09-12.
+//  Copyright (c) 2015 Arham Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: AnyObject?
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if let obj: AnyObject = dataObject {
+            self.dataLabel!.text = obj.description
+        } else {
+            self.dataLabel!.text = ""
+        }
+    }
+
+
+}
+
