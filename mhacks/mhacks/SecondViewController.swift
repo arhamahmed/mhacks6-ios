1bbc65c57510cf195aae65de5a0a21360bbefc4b
Arham Ahmed <a.arhamIT@gmail.com>
2015-09-13 00:38:45 -0400

    fixed second view
    correctly displays campaigns/locations tour buses are at

---
+++ mhacks/mhacks/SecondViewController.swift
@@ -0,0 +1,42 @@
+//
+//  SecondViewController.swift
+//  mhacks
+//
+//  Created by Arham Ahmed on 2015-09-12.
+//  Copyright (c) 2015 Arham Ahmed. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SecondViewController: UIViewController, UIPickerViewDataSource,  UIPickerViewDelegate {
+    
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    var pickerDataSource = ["Ann Arbor Michigan", "Toronto Ontario",
+                            "Waterloo Ontario", "Palo Alto California",
+                            "Quebec City Quebec", "Manhattan New York City"];
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.pickerView.dataSource = self;
+        self.pickerView.delegate = self;
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerDataSource.count;
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return pickerDataSource[row]
+    }
+}
